ce7b9b5ae1d5c8e7748a1719b613474f4625b7db
Meetali <2020.meetali.patil@ves.ac.in>
2023-02-28 20:02:14 +0530

    4

--- sw.js
+++ sw.js
@@ -1,7 +1,4 @@
-self.addEventListener("install", function (event) {
-    event.waitUntil(preLoad());
-    });
-    self.addEventListener("fetch", function (event) {
+self.addEventListener("fetch", function (event) {
     event.respondWith(checkResponse(event.request).catch(function () {
     console.log("Fetch from cache successful!")
     return returnFromCache(event.request);
@@ -9,66 +6,3 @@ self.addEventListener("install", function (event) {
     console.log("Fetch successful!")
     event.waitUntil(addToCache(event.request));
     });
-    self.addEventListener('sync', event => {
-    if (event.tag === 'syncMessage') {
-        
-        console.log("Sync successful!")
-}
-});
-self.addEventListener('push', function (event) {
-if (event && event.data) {
-var data = event.data.json();
-if (data.method == "pushMessage") {
-console.log("Push notification sent");
-event.waitUntil(self.registration.showNotification("Email Sent", {
-body: data.message
-}))
-}
-}
-})
-var filesToCache = [
-'/',
-'/images',
-'/cart.css',
-'/cart.js',
-'/form.html',
-'/index.html',
-'/products.js',
-'/success.html',
-];
-var preLoad = function () {
-return caches.open("offline").then(function (cache) {
-// caching index and important routes
-return cache.addAll(filesToCache);
-});
-};
-var checkResponse = function (request) {
-return new Promise(function (fulfill, reject) {
-fetch(request).then(function (response) {
-if (response.status !== 404) {
-fulfill(response);
-} else {
-reject();
-
-}
-}, reject);
-});
-};
-var addToCache = function (request) {
-return caches.open("offline").then(function (cache) {
-return fetch(request).then(function (response) {
-return cache.put(request, response);
-});
-});
-};
-var returnFromCache = function (request) {
-return caches.open("offline").then(function (cache) {
-return cache.match(request).then(function (matching) {
-if (!matching || matching.status == 404) {
-return cache.match("offline.html");
-} else {
-return matching;
-}
-});
-});
-};
